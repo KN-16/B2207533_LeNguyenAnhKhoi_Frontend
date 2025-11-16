27be4e7a58df1dfdef6b62aef77cb986dd29dde6
KN16 <lnakhoi5a@gmail.com>
2025-11-17 01:58:37 +0700

    Tao trang cap nhat lien he

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -15,14 +15,13 @@ async deleteAll() {
 return (await this.api.delete("/")).data;
 }
 async get(id) {
-return (await this.api.get('/${id}')).data;
+return (await this.api.get(`/${id}`)).data;
 }
 async update(id, data) {
-return (await this.api.put('/${id}', data)).data;
+return (await this.api.put(`/${id}`, data)).data;
 }
 async delete(id) {
-return (await this.api.delete('/${id}')).data;
-
+return (await this.api.delete(`/${id}`)).data;
 }
 }
 export default new ContactService();
